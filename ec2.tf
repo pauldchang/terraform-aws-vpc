d908fdb5c454d393c78a959d6841548aa5beadb6
pauldchang <153890789+pauldchang@users.noreply.github.com>
2024-05-06 20:27:43 -0500

    update module

---
+++ ec2.tf
@@ -0,0 +1,22 @@
+resource "aws_vpc" "my_vpc" {
+  cidr_block = var.cidr_block
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "10.0.1.0/24"
+
+  tags = {
+    Name = "public-subnet"
+  }
+}
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "10.0.2.0/24"
+
+
+  tags = {
+    Name = "private-subnet"
+  }
+}
